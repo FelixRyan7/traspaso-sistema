404cde6103dec60ad6d8489587394f6682942e7e
FelixRyan7 <fpasadase@uoc.edu>
2026-07-06 20:23:59 +0200

    add api limiter

--- Traspaso-bares-backend/src/app.js
+++ Traspaso-bares-backend/src/app.js
@@ -5,16 +5,20 @@ const errorMiddleware = require('./middlewares/error.middleware');
 const helmet = require("helmet");
 
 const authRoutes = require('./routes/auth.routes');
+const healthRoutes = require('./routes/health.routes')
 const dashboardRoutes = require('./routes/dashboard.routes');
 const locationRoutes = require('./routes/location.routes');
 const productRoutes = require('./routes/products.routes');
 const locationRequestRoutes = require('./routes/locationRequest.routes');
+const { apiLimiter } = require('./middlewares/rateLimit.middleware');
 
 const app = express();
 app.set("trust proxy", 1);
 app.use(helmet());
-
 app.use(corsMiddleware);
+app.use(apiLimiter);
+
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({
   extended: true,
@@ -24,6 +28,7 @@ app.use(cookieParser());
 
 // 🌐 rutas públicas
 app.use('/api/auth', authRoutes);
+app.use("/health", healthRoutes);
 // Rutas privadas
 app.use('/api/dashboard', dashboardRoutes)
 app.use('/api/locations', locationRoutes)
