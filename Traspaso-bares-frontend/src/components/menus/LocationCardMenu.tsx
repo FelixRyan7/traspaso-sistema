65ab3ce3cdce6b020f8e1897ddd4b1a6f1f86da7
FelixRyan7 <fpasadase@uoc.edu>
2026-07-20 17:01:20 +0200

    create new menu component for locatoin cards

---
+++ Traspaso-bares-frontend/src/components/menus/LocationCardMenu.tsx
@@ -0,0 +1,71 @@
+import { useTogglePos } from "../../hooks/PosHooks/useTogglePos";
+import type { Location } from "../../types/location";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+
+type Props = {
+  location: Location;
+  onEdit: (location: Location) => void;
+  onClose: () => void;
+};
+
+export default function LocationCardMenu({ location, onClose, onEdit }: Props) {
+  const { mutate: togglePos, isPending } = useTogglePos();
+
+  return (
+    <div
+      className="
+        absolute right-0 mt-2 z-20
+        w-56
+        bg-white/90 backdrop-blur-md
+        border border-gray-light/60
+        rounded-xl
+        shadow-lg
+        overflow-hidden
+      "
+    >
+      <button
+        onClick={() => {
+          onClose();
+          // navigate(...)
+        }}
+        className="flex items-center gap-3 w-full px-4 py-3 text-sm hover:bg-gray-light/50 transition"
+      >
+        <CalendarMonthOutlinedIcon fontSize="small" />
+        Gestion de traspasos
+      </button>
+
+      
+
+      <button
+        onClick={() => {
+          onEdit(location);
+        }}
+        className="flex items-center gap-3 w-full px-4 py-3 text-sm hover:bg-gray-light/50 transition"
+      >
+        <EditOutlinedIcon fontSize="small" />
+        Editar location
+      </button>
+
+      <div className="mx-2 my-1 border-t border-gray-light/70" />
+
+      <button
+        onClick={() => {
+          togglePos(location.id);
+          onClose();
+        }}
+        disabled={isPending}
+        className="flex items-center gap-3 w-full px-4 py-3 text-sm hover:bg-gray-light/50 transition"
+      >
+        <ToggleOffIcon fontSize="small" />
+        {isPending
+          ? "Actualizando..."
+          : location.isActive
+          ? "Desactivar"
+          : "Activar"}
+      </button>
+
+    </div>
+  );
+}
