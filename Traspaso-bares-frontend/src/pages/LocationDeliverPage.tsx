182d1350dc2a99a76e719e1c547407035c8d41e9
FelixRyan7 <fpasadase@uoc.edu>
2026-08-17 15:27:21 +0200

    change ui

--- Traspaso-bares-frontend/src/pages/LocationDeliverPage.tsx
+++ Traspaso-bares-frontend/src/pages/LocationDeliverPage.tsx
@@ -104,7 +104,7 @@ export default function LocationDirectDeliverPage() {
       />
 
       {/* GRID */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {filteredProducts.map((product) => (
           <LocationDeliverCard
             key={product.productId}
