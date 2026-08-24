033f5d014ead69cfc82ef6a5a6121fb044552110
FelixRyan7 <fpasadase@uoc.edu>
2026-08-24 16:03:24 +0200

    remove console log

--- Traspaso-bares-backend/src/config/cors.js
+++ Traspaso-bares-backend/src/config/cors.js
@@ -1,8 +1,6 @@
 const cors = require('cors');
 require('dotenv').config();
 
-console.log('🌐 FRONTEND_URL:', process.env.FRONTEND_URL);
-
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true
